d2a73e3ef26279d86611f8695421eac064b32af7
talhaadev <wpsetech@gmail.com>
2025-06-12 00:56:53 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations run successfully.';
 });
